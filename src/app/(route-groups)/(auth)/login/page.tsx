dd85874c9066c131ffdba1094188df8ffc600354
hasham0 <hashamsaleem75@gmail.com>
2025-08-24 02:41:26 +0500

    route groups done

---
+++ src/app/(route-groups)/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div>
+      <p>Login page</p>
+      <p className="text-gray-600 dark:text-gray-400 my-4 max-w-lg mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        dicta, vitae veritatis mollitia aliquid incidunt, atque, tempore
+        pariatur soluta officia modi laborum?
+      </p>
+    </div>
+  );
+}
---
+++ src/app/(route-groups)/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+export default function Register() {
+  return (
+    <div>
+      <p>Register page</p>
+      <p className="text-gray-600 dark:text-gray-400 my-4 max-w-lg mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        dicta, vitae veritatis mollitia aliquid incidunt, atque, tempore
+        pariatur soluta officia modi laborum?
+      </p>
+    </div>
+  );
+}
