2e5a0c3c234fcf0bf0700cd26378aff3aff7805c
hasham0 <hashamsaleem75@gmail.com>
2025-08-24 23:46:05 +0500

    unmatched routes

--- src/app/(parallel-routes)/complex-dashboard/@notifications/archived/page.tsx
+++ src/app/(parallel-routes)/complex-dashboard/@notifications/archived/page.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 import Card from "@/components/card";
 
-export default function Notifications() {
+export default function ArchivedNotifications() {
   return (
     <Card>
       <div className="flex flex-col gap-4">
-        <h3>Notifications</h3>
+        <h3>Archived Notifications</h3>
         <div>
           <Link
-            href="/complex-dashboard/archived"
+            href="/complex-dashboard"
             className="text-blue-500 hover:underline"
           >
-            Archived
+            Back to Notifications
           </Link>
         </div>
       </div>
