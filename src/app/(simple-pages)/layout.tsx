7ae651459d694b91b7dd532ee7ec0f1ac714bd93
hasham0 <hashamsaleem75@gmail.com>
2025-08-24 12:11:01 +0500

    simple route layout

---
+++ src/app/(simple-pages)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function SimpleLayout({ children }: Props) {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      {children}
+    </div>
+  );
+}
