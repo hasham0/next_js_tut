d0a46d79d41bb5aba227e03ae4e452c74c6b8423
hasham0 <hashamsaleem75@gmail.com>
2025-08-25 16:38:04 +0500

    server cookies

--- src/app/(server routes)/profile/api/route.ts
+++ src/app/(server routes)/profile/api/route.ts
@@ -1,10 +1,21 @@
-import { headers } from "next/headers";
+import { cookies } from "next/headers";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const headerList = await headers();
-  const token = headerList.get("authorization")?.replace("Bearer ", "");
+  // 1st way
+  // const theme = request.cookies.get("theme");
+  // console.log("🚀 ~ GET ~ theme:", theme);
+
+  // 2nd way
+  const cookieStore = await cookies();
+  cookieStore.set("result", "20");
+
+  const result = cookieStore.get("result");
+  console.log("result", result);
   return new Response("<h1>Profile</h1>", {
-    headers: { "Content-Type": "text/html" },
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
   });
 }
