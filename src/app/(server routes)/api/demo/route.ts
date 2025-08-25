c4bc8c6c43256e6cb0dd2a657a41d2e535f396f8
hasham0 <hashamsaleem75@gmail.com>
2025-08-25 14:05:37 +0500

    serach/query params

--- src/app/(server routes)/api/demo/route.ts
+++ src/app/(server routes)/api/demo/route.ts
@@ -3,11 +3,16 @@ import { NextResponse, type NextRequest } from "next/server";
 import comments from "@/app/(server routes)/comments/data";
 
 // GET Request
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
   return NextResponse.json(
     {
       message: "comments fetch successfully",
-      data: comments,
+      data: filteredComments,
     },
     {
       status: 200,
